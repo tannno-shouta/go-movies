4b08bb5f77785aa2d0e75e2a8e083de28e3674c2
tannno-shouta <71753010+tannno-shouta@users.noreply.github.com>
2022-05-26 18:45:03 +0900

    first commit
    環境構築と画面遷移を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+ 
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">
+            Go Watch a Movie!
+          </h1>
+          <hr className="mb-3" />
+        </div>
+ 
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin">Manage Catalog</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+ 
+          <div className="col-md-10">
+            <Routes>
+              <Route path="/movies">
+                <Movies />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
+ 
+const Home = () => {
+  return <h2>Home</h2>
+}
+ 
+const Movies = () => {
+  return <h2>Movies</h2>
+}
+const Admin = () => {
+  return <h2>Manage Catalog</h2>
+}
